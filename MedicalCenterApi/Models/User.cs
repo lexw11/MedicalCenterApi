cf8038e7134842da74592e2873f507889342e4d0
Alex Wright <36146160+lexw11@users.noreply.github.com>
2024-01-25 20:14:49 -0800

    Add project files.

---
+++ MedicalCenterApi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalCenterApi.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ MedicalCenterApi/Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MedicalCenterApi.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
