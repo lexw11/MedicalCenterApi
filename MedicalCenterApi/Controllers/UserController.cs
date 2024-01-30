ed816de3b25196b23240dced3a401665d522718a
Alex Wright <36146160+lexw11@users.noreply.github.com>
2024-01-29 20:27:53 -0800

    Implemented data transfer object to prevent over-posting

--- MedicalCenterApi/Controllers/UserController.cs
+++ MedicalCenterApi/Controllers/UserController.cs
@@ -1,123 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MedicalCenterApi.Models;
 
-namespace MedicalCenterApi.Controllers
+namespace MedicalCenterApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+    private readonly UserContext _context;
+
+    public UserController(UserContext context)
     {
-        private readonly UserContext _context;
-
-        public UserController(UserContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/User
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            return await _context.Users.ToListAsync();
-        }
-
-        // GET: api/User/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(long id)
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-        // PUT: api/User/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(long id, User user)
-        {
-            if (id != user.UserId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/User
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
-        {
-            if (_context.Users == null)
-            {
-                return Problem("Entity set 'UserContext.Users'  is null.");
-            }
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetUser), new { id = user.UserId }, user);
-        }
-
-        // DELETE: api/User/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(long id)
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool UserExists(long id)
-        {
-            return (_context.Users?.Any(e => e.UserId == id)).GetValueOrDefault();
-        }
+        _context = context;
     }
+
+    // GET: api/User
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserDTO>>> GetUsers()
+    {
+        return await _context.Users
+            .Select(user => UserToDTO(user))
+            .ToListAsync();
+    }
+
+    // GET: api/User/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDTO>> GetUser(long id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return UserToDTO(user);
+    }
+
+    // PUT: api/User/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutUser(long id, UserDTO userDTO)
+    {
+        if (id != userDTO.UserId)
+        {
+            return BadRequest();
+        }
+
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        { 
+            return NotFound(); 
+        }
+
+        user.EmailAddress = userDTO.EmailAddress;
+        user.FirstName = userDTO.FirstName;
+        user.LastName = userDTO.LastName;
+        user.Role = userDTO.Role;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) when (!UserExists(id))
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/User
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<UserDTO>> PostUser(UserDTO userDTO)
+    {
+        var user = new User
+        {
+            EmailAddress = userDTO.EmailAddress,
+            FirstName = userDTO.FirstName,
+            LastName = userDTO.LastName,
+            Role = userDTO.Role
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetUser), 
+            new { id = user.UserId }, 
+            UserToDTO(user));
+    }
+
+    // DELETE: api/User/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(long id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool UserExists(long id)
+    {
+        return (_context.Users?.Any(e => e.UserId == id)).GetValueOrDefault();
+    }
+
+    private static UserDTO UserToDTO(User user) =>
+        new UserDTO
+        {
+            UserId = user.UserId,
+            EmailAddress = user.EmailAddress,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Role = user.Role
+        };
 }
--- MedicalCenterApi/Models/UserDTO.cs
+++ MedicalCenterApi/Models/UserDTO.cs
@@ -1,13 +1,12 @@
 ﻿namespace MedicalCenterApi.Models
 {
-    public class User
+    public class UserDTO
     {
         public long UserId { get; set; }
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Role { get; set; }
-        public string? SSN { get; set; }
 
     }
 }
